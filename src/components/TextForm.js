0a8fadc9f3eb030aba66a3e596958031fa1368c7
Maha986 <59806957+Maha986@users.noreply.github.com>
2021-10-05 18:54:38 +0500

    done

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,23 +2,46 @@ import React,{useState} from "react";
 
 export default function TextForm(props) {
     const handleUpClick=()=>{
-        let newText=text.toUpperCase();
-        setText(newText);
-        props.showAlert("Converted to Uppercase!","success")
+        if(text.length>0)
+        {
+            let newText=text.toUpperCase();
+            setText(newText);
+            props.showAlert("Converted to Uppercase!","success")
+        }
+        else
+        {
+            props.showAlert("Please enter some text for conversion","warning")
+        }
     }
     const onChangeClick=(event)=>{
         setText(event.target.value);
     }
     const handleLowClick=()=>{
-        let newText=text.toLowerCase();
-        setText(newText);
-        props.showAlert("Converted to Lowercase!","success")
+        if(text.length>0)
+        {
+            let newText=text.toLowerCase();
+            setText(newText);
+            props.showAlert("Converted to Lowercase!","success")
+        }
+        else
+        {
+            props.showAlert("Please enter some text for conversion","warning")
+        }
+        
     }
     const handleCamClick=()=>{
-        let newText=text.toLowerCase();
-        newText=newText.replace(newText[0],newText[0].toUpperCase());
-        setText(newText);
-        props.showAlert("Text Capatilized!","success")
+        if(text.length>0)
+        {
+            let newText=text.toLowerCase();
+            newText=newText.replace(newText[0],newText[0].toUpperCase());
+            setText(newText);
+            props.showAlert("Text Capatilized!","success")
+        }
+        else
+        {
+            props.showAlert("Please enter some text for conversion","warning")
+        }
+        
     }
     const [text,setText]=useState("");
     // let wordcount;
@@ -44,7 +67,8 @@ export default function TextForm(props) {
         <div className="mb-3">
             <h1>Your Text Summary</h1>
             {/* way to use the if else using ternary operator */}
-            <p>{text.split(" ")[text.split(" ").length-1]===""?text.split(" ").length-1:text.split(" ").length} word(s) and {text.length} character(s)</p>
+            <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} word(s) and {text.length} character(s)</p>
+            {/* <p>{text.split(" ")[text.split(" ").length-1]===""?text.split(" ").length-1:text.split(" ").length} word(s) and {text.length} character(s)</p> */}
         </div>
         </>
     );
